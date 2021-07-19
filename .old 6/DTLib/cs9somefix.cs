bc925cca6177a6541e1b3c79e17d861b02a89413
Timerix22 <87668556+Timerix22@users.noreply.github.com>
2021-07-19 21:35:46 +0300

    Delete .old 6 directory

--- .old 6/DTLib/cs9somefix.cs
+++
@@ -1,8 +0,0 @@
-﻿// включает init и record из c# 9.0
-using System.ComponentModel;
-
-namespace System.Runtime.CompilerServices
-{
-    [EditorBrowsable(EditorBrowsableState.Never)]
-    internal class IsExternalInit { }
-}
